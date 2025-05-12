f855803b14cc7f49176c5dfc393aae6fcfa71468
SyeedBilal <syeedbilalkirmaney@gmail.com>
2025-05-12 16:20:29 +0530

    added cart and order in persistant config

--- frontend/src/services/persistantConfig.js
+++ frontend/src/services/persistantConfig.js
@@ -3,6 +3,6 @@ import storage from 'redux-persist/lib/storage';
 export const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['auth','admin','adminItems'], 
+  whitelist: ['auth','admin','adminItems','cart','order'], 
  
 };
