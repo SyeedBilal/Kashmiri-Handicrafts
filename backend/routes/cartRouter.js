43bf7991df8c8812386f4b719663d4dc5b1fc6ce
SyeedBilal <syeedbilalkirmaney@gmail.com>
2025-05-12 15:09:28 +0530

    debugging

--- backend/routes/cartRouter.js
+++ backend/routes/cartRouter.js
@@ -3,7 +3,7 @@ const CartRouter = express.Router();
 const cartController = require('../controllers/cartController');
 const {protectUser} = require('../Middlewares/protectUser');
 
-CartRouter.get('/api/cart-items', protectUser, cartController.getCartItems);
+CartRouter.get('/api/cart-items', cartController.getCartItems);
 CartRouter.post('/api/add-to-cart', protectUser, cartController.addToCart);
 CartRouter.post('/api/cart/remove', protectUser, cartController.removeFromCart);
 
